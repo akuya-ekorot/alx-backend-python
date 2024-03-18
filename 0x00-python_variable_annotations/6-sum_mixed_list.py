34c87f5b7287f65430668abf1f6b5e374b975b25
Akuya Ekorot <akuyaekorot@gmail.com>
2024-03-18 14:48:20 +0300

    change order of union types

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,6 +4,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''Return the sum of the mixed list.'''
     return sum(mxd_lst)
